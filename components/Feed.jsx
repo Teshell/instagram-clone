851755fdf8b84758e7584d871eee43ff449d5c38
Teshell <fayrongeek@gmail.com>
2022-04-18 17:08:47 +0100

    building the image section of the post

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,3 +1,4 @@
+import Posts from "./Posts";
 import Stories from "./Stories";
 
 const Feed = () => {
@@ -5,7 +6,7 @@ const Feed = () => {
     <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
       <section className="col-span-2">
         <Stories />
-        {/* Posts */}
+        <Posts />
       </section>
 
       <section>
---
+++ components/Posts.jsx
@@ -0,0 +1,46 @@
+import Post from "./Post";
+
+const posts = [
+  {
+    id: "123",
+    username: "zinedine_hamadi",
+    userImg: "https://links.papareact.com/3ke",
+    img: "https://links.papareact.com/3ke",
+    caption: "This is DOPE!!",
+  },
+
+  {
+    id: "123",
+    username: "zinedine_hamadi",
+    userImg: "https://links.papareact.com/3ke",
+    img: "https://links.papareact.com/3ke",
+    caption: "This is DOPE!!",
+  },
+
+  {
+    id: "123",
+    username: "zinedine_hamadi",
+    userImg: "https://links.papareact.com/3ke",
+    img: "https://links.papareact.com/3ke",
+    caption: "This is DOPE!!",
+  },
+];
+
+const Posts = () => {
+  return (
+    <div>
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          id={post.id}
+          username={post.username}
+          userImg={post.userImg}
+          img={post.img}
+          caption={post.caption}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Posts;
